9ff92d1155bfcbea1d2fe3d29fc64ceeab5f9d99
榊原昌彦 <sakakibara@rdlabo.jp>
2025-06-03 12:48:36 +0900

    feat(signal): add error case

--- src/rules/signal-use-as-signal.ts
+++ src/rules/signal-use-as-signal.ts
@@ -1,4 +1,6 @@
 import { TSESLint } from '@typescript-eslint/utils';
+import { RuleFix } from '@typescript-eslint/utils/dist/ts-eslint';
+import { TSESTree } from '@typescript-eslint/types';
 
 const rule: TSESLint.RuleModule<'signalUseAsSignal', []> = {
   defaultOptions: [],
@@ -7,7 +9,7 @@ const rule: TSESLint.RuleModule<'signalUseAsSignal', []> = {
       description: 'This plugin check to valid signal use as signal.',
       url: '',
     },
-    fixable: undefined,
+    fixable: 'code',
     messages: {
       signalUseAsSignal:
         'signals should not be used as signals: `{{ identifier }}`',
@@ -17,6 +19,7 @@ const rule: TSESLint.RuleModule<'signalUseAsSignal', []> = {
   },
   create: (context) => {
     const signalIdentifiers = new Set<string>();
+    const readonlySignalIdentifiers = new Set<string>();
 
     return {
       PropertyDefinition(node) {
@@ -28,15 +31,41 @@ const rule: TSESLint.RuleModule<'signalUseAsSignal', []> = {
         ) {
           if (node.key.type === 'PrivateIdentifier') {
             signalIdentifiers.add(node.key.name);
+          } else if (node.key.type === 'Identifier') {
+            signalIdentifiers.add(node.key.name);
           }
+        }
+        // asReadonly()で生成されたreadonly signal
+        if (
+          node.value &&
+          node.value.type === 'CallExpression' &&
+          node.value.callee.type === 'MemberExpression' &&
+          node.value.callee.property.type === 'Identifier' &&
+          node.value.callee.property.name === 'asReadonly' &&
+          (node.key.type === 'PrivateIdentifier' ||
+            node.key.type === 'Identifier')
+        ) {
+          readonlySignalIdentifiers.add(node.key.name);
         }
       },
 
+      // this.#signal または this.signal の直接利用
       MemberExpression(node) {
+        // AssignmentExpressionの左辺（this.#id = ...）で、this.#idやthis.userなどsignal本体のみスキップ
+        if (
+          node.parent &&
+          node.parent.type === 'AssignmentExpression' &&
+          node.parent.left === node &&
+          node.object.type === 'ThisExpression'
+        ) {
+          return;
+        }
         if (
           node.object.type === 'ThisExpression' &&
-          node.property.type === 'PrivateIdentifier' &&
-          signalIdentifiers.has(node.property.name)
+          ((node.property.type === 'PrivateIdentifier' &&
+            signalIdentifiers.has(node.property.name)) ||
+            (node.property.type === 'Identifier' &&
+              signalIdentifiers.has(node.property.name)))
         ) {
           const parent = node.parent;
           if (
@@ -50,8 +79,200 @@ const rule: TSESLint.RuleModule<'signalUseAsSignal', []> = {
               data: {
                 identifier: `this.${node.property.name}`,
               },
+              fix: (fixer) => {
+                const fixes: RuleFix[] = [];
+                fixes.push(fixer.insertTextAfter(node.property, '()'));
+                return fixes;
+              },
             });
           }
+        }
+        // this.#signal().hoge = ... または this.signal().hoge = ...
+        if (
+          node.object.type === 'CallExpression' &&
+          node.object.callee.type === 'MemberExpression' &&
+          node.object.callee.object.type === 'ThisExpression' &&
+          ((node.object.callee.property.type === 'PrivateIdentifier' &&
+            (signalIdentifiers.has(node.object.callee.property.name) ||
+              readonlySignalIdentifiers.has(
+                node.object.callee.property.name
+              ))) ||
+            (node.object.callee.property.type === 'Identifier' &&
+              (signalIdentifiers.has(node.object.callee.property.name) ||
+                readonlySignalIdentifiers.has(
+                  node.object.callee.property.name
+                ))))
+        ) {
+          if (
+            node.parent &&
+            node.parent.type === 'AssignmentExpression' &&
+            node.parent.left === node
+          ) {
+            context.report({
+              node: node,
+              messageId: 'signalUseAsSignal',
+              data: {
+                identifier: `this.${node.object.callee.property.name}()`,
+              },
+              fix: (fixer) => {
+                const fixes: RuleFix[] = [];
+                const callExpression = node.object as TSESTree.CallExpression;
+                const memberExpression =
+                  callExpression.callee as TSESTree.MemberExpression;
+                let signalName = '';
+                if (memberExpression.property.type === 'Identifier') {
+                  signalName = memberExpression.property.name;
+                } else if (
+                  memberExpression.property.type === 'PrivateIdentifier'
+                ) {
+                  signalName = `#${memberExpression.property.name}`;
+                }
+                const propertyName =
+                  node.property.type === 'Identifier' ? node.property.name : '';
+                const assignmentNode =
+                  node.parent as TSESTree.AssignmentExpression;
+                fixes.push(
+                  fixer.replaceText(
+                    node.parent,
+                    `this.${signalName}.update(value => ({ ...value, ${propertyName}: ${context.getSourceCode().getText(assignmentNode.right)}}))`
+                  )
+                );
+                return fixes;
+              },
+            });
+          }
+          // push等の破壊的メソッド呼び出し
+          if (
+            node.parent &&
+            node.parent.type === 'CallExpression' &&
+            node.parent.callee === node
+          ) {
+            context.report({
+              node: node,
+              messageId: 'signalUseAsSignal',
+              data: {
+                identifier: `this.${node.object.callee.property.name}()`,
+              },
+              fix: (fixer) => {
+                const fixes: RuleFix[] = [];
+                const callExpression = node.object as TSESTree.CallExpression;
+                const memberExpression =
+                  callExpression.callee as TSESTree.MemberExpression;
+                let signalName = '';
+                if (memberExpression.property.type === 'Identifier') {
+                  signalName = memberExpression.property.name;
+                } else if (
+                  memberExpression.property.type === 'PrivateIdentifier'
+                ) {
+                  signalName = `#${memberExpression.property.name}`;
+                }
+                const methodName =
+                  node.property.type === 'Identifier' ? node.property.name : '';
+                const callNode = node.parent as TSESTree.CallExpression;
+                const args = callNode.arguments
+                  .map((arg) => context.getSourceCode().getText(arg))
+                  .join(', ');
+                fixes.push(
+                  fixer.replaceText(
+                    node.parent,
+                    `this.${signalName}.update(value => { value.${methodName}(${args}); return value; })`
+                  )
+                );
+                return fixes;
+              },
+            });
+          }
+        }
+      },
+
+      // this.#signal() = ... または this.signal() = ... のような直接代入
+      AssignmentExpression(node) {
+        // this.#signal() = ... または this.signal() = ... のような直接代入
+        if (
+          node.left.type === 'CallExpression' &&
+          node.left.callee.type === 'MemberExpression' &&
+          node.left.callee.object.type === 'ThisExpression' &&
+          ((node.left.callee.property.type === 'PrivateIdentifier' &&
+            (signalIdentifiers.has(node.left.callee.property.name) ||
+              readonlySignalIdentifiers.has(node.left.callee.property.name))) ||
+            (node.left.callee.property.type === 'Identifier' &&
+              (signalIdentifiers.has(node.left.callee.property.name) ||
+                readonlySignalIdentifiers.has(node.left.callee.property.name))))
+        ) {
+          context.report({
+            node: node.left,
+            messageId: 'signalUseAsSignal',
+            data: {
+              identifier: `this.${node.left.callee.property.name}()`,
+            },
+            fix: (fixer) => {
+              const fixes: RuleFix[] = [];
+              const callExpression = node.left as TSESTree.CallExpression;
+              const memberExpression =
+                callExpression.callee as TSESTree.MemberExpression;
+              let signalName = '';
+              if (memberExpression.property.type === 'Identifier') {
+                signalName = memberExpression.property.name;
+              } else if (
+                memberExpression.property.type === 'PrivateIdentifier'
+              ) {
+                signalName = `#${memberExpression.property.name}`;
+              }
+              fixes.push(
+                fixer.replaceText(
+                  node,
+                  `this.${signalName}.set(${context.getSourceCode().getText(node.right)})`
+                )
+              );
+              return fixes;
+            },
+          });
+        }
+        // this.#signal = ... または this.signal = ... のような直接代入
+        else if (
+          node.left.type === 'MemberExpression' &&
+          node.left.object.type === 'ThisExpression' &&
+          ((node.left.property.type === 'PrivateIdentifier' &&
+            signalIdentifiers.has(node.left.property.name)) ||
+            (node.left.property.type === 'Identifier' &&
+              signalIdentifiers.has(node.left.property.name)))
+        ) {
+          // 右辺がsignal関数の呼び出しで、型が同じ場合はスキップ
+          if (
+            node.right.type === 'CallExpression' &&
+            node.right.callee.type === 'Identifier' &&
+            ['signal', 'model'].includes(node.right.callee.name)
+          ) {
+            return;
+          }
+
+          const memberExpr = node.left as TSESTree.MemberExpression;
+          context.report({
+            node: memberExpr,
+            messageId: 'signalUseAsSignal',
+            data: {
+              identifier: (() => {
+                if (memberExpr.property.type === 'Identifier') {
+                  return `this.${memberExpr.property.name}`;
+                } else if (memberExpr.property.type === 'PrivateIdentifier') {
+                  return `this.#${memberExpr.property.name}`;
+                }
+                return 'this.';
+              })(),
+            },
+            fix: (fixer) => {
+              let signalName = '';
+              if (memberExpr.property.type === 'Identifier') {
+                signalName = memberExpr.property.name;
+              } else if (memberExpr.property.type === 'PrivateIdentifier') {
+                signalName = `#${memberExpr.property.name}`;
+              }
+              return fixer.replaceText(
+                node,
+                `this.${signalName}.set(${context.getSourceCode().getText(node.right)})`
+              );
+            },
+          });
         }
       },
     };
--- tests/rules/signal-use-as-signal.ts
+++ tests/rules/signal-use-as-signal.ts
@@ -7,6 +7,31 @@ new RuleTester().run('signal-use-as-signal', rule, {
       code: `
         @Component()
         export class SigninPage {
+          #user = signal<{ name: string }>({ name: 'John' });
+        
+          updateUser() {
+            this.#user = signal<{ name: string }>({ name: 'Perry' });
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            const user = this.#user();
+            user.name = 'Jane';
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
           readonly #id = signal<number>(undefined);
 
           constructor() {
@@ -17,6 +42,95 @@ new RuleTester().run('signal-use-as-signal', rule, {
             if (this.#id()) {
               this.#id.set(2);
             }
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.#user.update(user => ({ ...user, name: 'Jane' }));
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #count = model<number>(0);
+
+          increment() {
+            this.#count.update(count => count + 1);
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+          readonly #readonlyUser = this.#user.asReadonly();
+
+          useReadonly() {
+            const name = this.#readonlyUser().name;
+          }
+        }
+      `,
+    },
+    // public signal
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(user => ({ ...user, name: 'Jane' }));
+          }
+        }
+      `,
+    },
+    // soft private signal
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          private readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(user => ({ ...user, name: 'Jane' }));
+          }
+        }
+      `,
+    },
+    // protected signal
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          protected readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(user => ({ ...user, name: 'Jane' }));
+          }
+        }
+      `,
+    },
+    // static signal
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          static readonly user = signal<{ name: string }>({ name: 'John' });
+
+          static updateUser() {
+            this.user.update(user => ({ ...user, name: 'Jane' }));
           }
         }
       `,
@@ -43,7 +157,214 @@ new RuleTester().run('signal-use-as-signal', rule, {
       errors: [
         { messageId: 'signalUseAsSignal', line: 7 },
         { messageId: 'signalUseAsSignal', line: 11 },
+        { messageId: 'signalUseAsSignal', line: 12 },
       ],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly #id = signal<number>(undefined);
+          
+          constructor() {
+            this.#id.set(1);
+          }
+          
+          useMethod() {
+            if (this.#id()) {
+              this.#id.update(value => ({ ...value, hoge: 1}));
+            }
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.#user().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.#user.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #count = model<number>(0);
+
+          increment() {
+            this.#count() = this.#count() + 1;
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly #count = model<number>(0);
+
+          increment() {
+            this.#count.set(this.#count() + 1);
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+          readonly #readonlyUser = this.#user.asReadonly();
+
+          updateReadonly() {
+            this.#readonlyUser().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 8 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly #user = signal<{ name: string }>({ name: 'John' });
+          readonly #readonlyUser = this.#user.asReadonly();
+
+          updateReadonly() {
+            this.#readonlyUser.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
+    },
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly #numbers = signal<number[]>([1, 2, 3]);
+
+          updateNumbers() {
+            this.#numbers().push(4);
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly #numbers = signal<number[]>([1, 2, 3]);
+
+          updateNumbers() {
+            this.#numbers.update(value => { value.push(4); return value; });
+          }
+        }
+      `,
+    },
+    // public signalの不正な使用
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
+    },
+    // soft private signalの不正な使用
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          private readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          private readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
+    },
+    // protected signalの不正な使用
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          protected readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          protected readonly user = signal<{ name: string }>({ name: 'John' });
+
+          updateUser() {
+            this.user.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
+    },
+    // static signalの不正な使用
+    {
+      code: `
+        @Component()
+        export class SigninPage {
+          static readonly user = signal<{ name: string }>({ name: 'John' });
+
+          static updateUser() {
+            this.user().name = 'Jane';
+          }
+        }
+      `,
+      errors: [{ messageId: 'signalUseAsSignal', line: 7 }],
+      output: `
+        @Component()
+        export class SigninPage {
+          static readonly user = signal<{ name: string }>({ name: 'John' });
+
+          static updateUser() {
+            this.user.update(value => ({ ...value, name: 'Jane'}));
+          }
+        }
+      `,
     },
   ],
 });
