bcf1b7319b1a00317851d2e1bb585e228010d448
榊原昌彦 <sakakibara@rdlabo.jp>
2025-09-14 19:20:03 +0900

    chore

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,14 @@
 // @ts-check
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
-const plugin = require('./dist/index.js');
 
-module.exports = tseslint.config(
-  {
-    files: ['**/*.ts'],
-    ignores: ['!.*.js', '/node_modules', '/dist', '/scripts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-    ],
-    rules: {},
-  },
-  {
-    files: ['**/*.html'],
-    ignores: ['/node_modules', '/dist'],
-    languageOptions: {
-      parser: require('@angular-eslint/template-parser'),
-    },
-    plugins: {
-      '@rdlabo/rules': plugin,
-    },
-    rules: {
-      '@rdlabo/rules/no-string-boolean-ionic-attr': 'error',
-    },
-  }
-);
+module.exports = tseslint.config({
+  files: ['**/*.ts'],
+  ignores: ['!.*.js', '/node_modules', '/dist', '/scripts'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+  ],
+  rules: {},
+});
